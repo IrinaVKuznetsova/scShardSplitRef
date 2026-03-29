c77dcd0d7274f7e4a514ebf87a46b44e80f9f607
Adam H. Sparks <adamhsparks@icloud.com>
2026-03-29 16:03:00 +0800

    convert to base R, no dplyr, fix bug in fn name call on line 129

--- R/00_process_centromere_split.r
+++ R/00_process_centromere_split.r
@@ -11,8 +11,6 @@
 #' @param pt_name A string representing the name of the plastid chromosome
 #'  (*e.g.*, "Pt").
 #'
-#' @import dplyr
-#' @import stringr
 #' @importFrom utils read.table write.table
 #'
 #' @examples
@@ -22,44 +20,45 @@
 #'                        pt_name = "Pt")
 #'
 #' @author Irina Kuznetsova, \email{irina.Kuznetsova@@curtin.edu.au}
-#' @export
+#' @dev
 #'
-#################################################################################################################################################
-# FUNCTION-1 checks for the rare case where a gene feature lies on the centromere coordinate and recalculates the split coordinate.
-# It takes the end coordinate of any gene that overlaps the centromere and shifts it 10 bp downstream to establish a new split coordinate.
+################################################################################
+# FUNCTION-1 checks for the rare case where a gene feature lies on the
+# centromere coordinate and recalculates the split coordinate.
+# It takes the end coordinate of any gene that overlaps the centromere and
+# shifts it 10 bp downstream to establish a new split coordinate.
 #
-# FUNCTION-0 prepares centromere coordinates in the correct format for FASTA splitting.
-#################################################################################################################################################
+# FUNCTION-0 prepares centromere coordinates in the correct format for FASTA
+# splitting.
+################################################################################
 
 prepare_centromere_BED = function(df, contig_prefix, mt_name, pt_name) {
-  df_result = df %>%
-    rowwise() %>%
-    do({
-      chr = .$Chr
-      start = .$CentrStart
-      end = .$ChrEND
+  contig <- grepl(sprintf("^%s", contig_prefix), df$Chr)
+  organelle <- df$Chr %in% c(mt_name, pt_name)
+  to_keep <- contig | organelle
 
-      # Identify contigs and Mt and Pt
-      contig = str_detect(chr, paste0("^", contig_prefix))
-      organelle = chr %in% c(mt_name, pt_name)
-      # check if either condition is TRUE
-      if (contig == TRUE || organelle == TRUE) {
-        # Keep unchanged
-        data.frame(Chr = chr, CentrStart = start, ChrEND = end)
-      } else {
-        # Split only if CentrStart > 0
-        if (start > 0) {
-          data.frame(
-            Chr = chr,
-            CentrStart = c(0, start),
-            ChrEND = c(start, end)
-          )
-        } else {
-          data.frame(Chr = chr, CentrStart = start, ChrEND = end)
-        }
-      }
-    }) %>%
-    ungroup()
+  split_rows <- (!to_keep) & (df$CentrStart > 0L)
+  unchanged_rows <- to_keep | (!to_keep & df$CentrStart <= 0L)
+
+  df_unchanged <- df[unchanged_rows, , drop = FALSE]
+
+  if (any(split_rows)) {
+    chr_split <- df$Chr[split_rows]
+    start_split <- df$CentrStart[split_rows]
+    end_split <- df$ChrEND[split_rows]
+
+    df_split <- data.frame(
+      Chr = rep(chr_split, each = 2L),
+      CentrStart = as.vector(rbind(0L, start_split)),
+      ChrEND = as.vector(rbind(start_split, end_split))
+    )
+
+    df_result <- rbind(df_unchanged, df_split)
+    rownames(df_result) <- NULL
+  } else {
+    df_result <- df_unchanged
+  }
+
   return(df_result)
 }
 
@@ -79,8 +78,6 @@ prepare_centromere_BED = function(df, contig_prefix, mt_name, pt_name) {
 #' @param pt_name A string representing the name of the plastid chromosome
 #'  (*e.g.*, "Pt").
 #'
-#' @import dplyr
-#' @import stringr
 #' @importFrom utils read.table write.table
 #'
 #' @examples
@@ -93,8 +90,7 @@ prepare_centromere_BED = function(df, contig_prefix, mt_name, pt_name) {
 #'
 #' @author Irina Kuznetsova, \email{irina.Kuznetsova@@curtin.edu.au}
 #' @export
-#'
-prepare_centromere_split = function(
+prepare_centromere_split <- function(
   gtf_path,
   centromere_path,
   out_path,
@@ -103,10 +99,10 @@ prepare_centromere_split = function(
   pt_name
 ) {
   # --------------------------------------------------
-  # 1 Read GTF file, add column names
+  # 1. Read GTF file, add column names
   # --------------------------------------------------
-  gtf_file = read.table(file = gtf_path, header = FALSE, sep = "\t")
-  colnames(gtf_file) = c(
+  gtf_file <- read.table(file = gtf_path, header = FALSE, sep = "\t")
+  colnames(gtf_file) <- c(
     "Chr",
     "source",
     "feature",
@@ -117,16 +113,16 @@ prepare_centromere_split = function(
     "frame",
     "attribute"
   )
-  gtf_file$unique_ID = paste0("ID", seq_len(nrow(gtf_file)))
+  gtf_file$unique_ID <- sprintf("ID%d", seq_len(nrow(gtf_file)))
 
   # --------------------------------------------------
-  # 2 Read centromere positions file (BED format)
+  # 2. Read centromere positions file (BED format)
   # --------------------------------------------------
-  read_cent = read.table(file = centromere_path, sep = "\t")
-  colnames(read_cent) = c("Chr", "CentrStart", "ChrEND")
+  read_cent <- read.table(file = centromere_path, sep = "\t")
+  colnames(read_cent) <- c("Chr", "CentrStart", "ChrEND")
 
-  # Convert original BED to suit FASTA splitting
-  orig_For_FASTA = prep_centromere_BED(
+  # Convert original BED format ready for FASTA processing
+  orig_For_FASTA <- prepare_centromere_BED(
     read_cent,
     contig_prefix,
     mt_name,
@@ -134,7 +130,7 @@ prepare_centromere_split = function(
   )
   write.table(
     orig_For_FASTA,
-    file = paste0(out_path, "IN0_original_centromeres_for_FASTA.bed"),
+    file = sprintf("%sIN0_original_centromeres_for_FASTA.bed", out_path),
     quote = FALSE,
     row.names = FALSE,
     col.names = FALSE,
@@ -142,50 +138,43 @@ prepare_centromere_split = function(
   )
 
   # ---------------------------------------------------------------------------
-  # 3 Combine two data frames
+  # 3. Combine the two data frames
   # --------------------------------------------------------------------------
   df_merged_chr <- merge(gtf_file, read_cent, by = "Chr")
 
   # --------------------------------------------------------------------------
-  # 4 Check for overlapping gene features
+  # 4. Check for overlapping gene features (vectorised)
   # --------------------------------------------------------------------------
-  check_genefeat_centr_overlap = df_merged_chr %>%
-    dplyr::filter(start < CentrStart & end > CentrStart)
-
-  # if (nrow(check_genefeat_centr_overlap) > 0){
-  #   print(c(paste("g_start:", check_genefeat_centr_overlap$start,
-  #           "g_end:", check_genefeat_centr_overlap$end,
-  #           "chr_Start:", check_genefeat_centr_overlap$CentrStart,
-  #           "chr_End:", check_genefeat_centr_overlap$ChrEND, sep = " "),
-  #           "Attention this genome has gene features coordinates overlaping with the centromere cooridnate in",
-  #           check_genefeat_centr_overlap$Chr[1]    ))
-  # }
+  check_genefeat_centr_overlap <-
+    df_merged_chr[
+      df_merged_chr[["start"]] < df_merged_chr[["CentrStart"]] &
+        df_merged_chr[["end"]] > df_merged_chr[["CentrStart"]],
+    ]
 
   # Prepare empty object to store new split coordinates
   BEDnew_split_coord_for_overlap_genes_with_centrom <- data.frame()
 
   # --------------------------------------------------------------------------
-  # 5 Handle overlaps
+  # 5. Handle overlaps
   # --------------------------------------------------------------------------
   if (nrow(check_genefeat_centr_overlap) > 0) {
-    message(
-      "\nOverlapping gene features with centromere coordinates detected!\n",
-      "re-calculating split coordinates for these chromosomes\n",
-      "Use:\n",
-      out_path,
-      "RECAL1_FINAL_SPLIT_coord_for_GTF.bed as the input to modify GTF file.\n",
-      "and:\n",
-      out_path,
-      "RECAL1_FINAL_SPLIT_coord_for_FASTA.bed",
-      "\n as the input for splitting FASTA."
+    cli::cli_alert_warning(
+      "{.strong Overlapping gene features with centromere coordinates detected!}
+      Re-calculating split coordinates for these chromosomes.
+
+      Use:
+        {.file {sprintf('%sRECAL1_FINAL_SPLIT_coord_for_GTF.bed', out_path)}}
+        as the input to modify the GTF file; and
+
+        {.file {sprintf('%sRECAL1_FINAL_SPLIT_coord_for_FASTA.bed', out_path)}}
+        as the input for splitting FASTA."
     )
 
     # 5.1 Shift centromere split coordinate 10 bp downstream of the gene feature end coordinate
-    new_split_coord_for_overlap_genes_with_centrom = check_genefeat_centr_overlap$end +
-      10
+    new_split_coord <- check_genefeat_centr_overlap$end + 10L
 
     # 5.2 Create new data frame with updated split coordinates
-    new_split_coord_for_overlap_genes_with_centrom = data.frame(
+    new_overlap_df <- data.frame(
       Chr = check_genefeat_centr_overlap$Chr,
       source = check_genefeat_centr_overlap$source,
       feature = check_genefeat_centr_overlap$feature,
@@ -194,46 +183,41 @@ prepare_centromere_split = function(
       score = check_genefeat_centr_overlap$score,
       strand = check_genefeat_centr_overlap$strand,
       frame = check_genefeat_centr_overlap$frame,
-      attached = check_genefeat_centr_overlap$attribute,
+      attribute = check_genefeat_centr_overlap$attribute,
       unique_ID = check_genefeat_centr_overlap$unique_ID,
-      CentrStart = new_split_coord_for_overlap_genes_with_centrom,
+      CentrStart = new_split_coord,
       ChrEND = check_genefeat_centr_overlap$ChrEND
     )
-    # 5.3 Write out new split coordinates for genes overlapping centromeres
-    # write.table(new_split_coord_for_overlap_genes_with_centrom,
-    #    file = paste0(out_path, "test_overlap.bed"),
-    #    quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-    # 5.4 Prepare final BED with only Chr, CentrStart, ChrEND columns
-    BEDnew_split_coord_for_overlap_genes_with_centrom = data.frame(
-      Chr = new_split_coord_for_overlap_genes_with_centrom$Chr,
-      CentrStart = new_split_coord_for_overlap_genes_with_centrom$CentrStart,
-      ChrEND = new_split_coord_for_overlap_genes_with_centrom$ChrEND
-    ) %>%
-      dplyr::distinct()
+
+    # 5.4 Prepare the final BED with only Chr, CentrStart, ChrEND columns and drop duplicates
+    tmp_bed <- unique(new_overlap_df[, c("Chr", "CentrStart", "ChrEND")])
 
     # --------------------------------------------------------------------------
-    # 6 Combine final BED coordinates
+    # 6. Combine final BED coordinates
     # --------------------------------------------------------------------------
     # 6.1 Get rows for chromosomes without overlapping genes
-    df_exact_centromere_split = read_cent[
-      read_cent$Chr %in%
-        BEDnew_split_coord_for_overlap_genes_with_centrom$Chr ==
-        FALSE,
-    ]
-    # 6.2 Combine
-    BED_df_combined = rbind(
-      df_exact_centromere_split,
-      BEDnew_split_coord_for_overlap_genes_with_centrom
-    )
-    # 6.3 Sort as original bed file
-    BED_df_combined_sorted_as_original = BED_df_combined[
-      match(read_cent$Chr, BED_df_combined$Chr),
-    ]
+    non_overlap_chr <- !(read_cent$Chr %in% tmp_bed$Chr)
+    df_exact_centromere_split <- read_cent[non_overlap_chr, ]
 
+    # 6.2 Combine
+    BED_df_combined <- rbind(df_exact_centromere_split, tmp_bed)
+
+    # 6.3 Sort as original BED file order
+    order_idx <- match(read_cent$Chr, BED_df_combined$Chr)
+    BED_df_combined_sorted_as_original <- BED_df_combined[order_idx, ]
+
+    gtf_bed_outfile <- sprintf(
+      "%sRECAL1_FINAL_SPLIT_coord_for_GTF.bed",
+      out_path
+    )
+    fasta_bed_outfile <- sprintf(
+      "%sRECAL1_FINAL_SPLIT_coord_for_FASTA.bed",
+      out_path
+    )
     # for GTF
     write.table(
       BED_df_combined_sorted_as_original,
-      file = paste0(out_path, "RECAL1_FINAL_SPLIT_coord_for_GTF.bed"),
+      file = gtf_bed_outfile,
       quote = FALSE,
       row.names = FALSE,
       col.names = FALSE,
@@ -241,45 +225,27 @@ prepare_centromere_split = function(
     )
     # for FASTA
     write.table(
-      prep_centromere_BED(
+      prepare_centromere_BED(
         BED_df_combined_sorted_as_original,
         contig_prefix,
         mt_name,
         pt_name
       ),
-      file = paste0(out_path, "RECAL1_FINAL_SPLIT_coord_for_FASTA.bed"),
+      file = fasta_bed_outfile,
       quote = FALSE,
       row.names = FALSE,
       col.names = FALSE,
       sep = "\t"
     )
   } else {
-    #5.5 Write out BED with only Chr, CentrStart, ChrEND columns
-    # write.table(BEDnew_split_coord_for_overlap_genes_with_centrom,
-    #   file = paste0(out_path, "test.bed"),
-    #   quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-    message(
-      " \n",
-      "No overlapping gene features detected use the original centromere coordinates BED file.\n",
-      "Use:\n",
-      centromere_path,
-      " as the input to modify GTF file.\n",
-      "and:\n",
-      out_path,
-      "IN0_original_centromeres_for_FASTA.bed",
-      " for splitting FASTA.",
+    cli::cli_alert_success(
+      "No overlapping gene features detected.
+      Use:
+        {.file {centromere_path}}
+      as the input to modify GTF file.
+      and:
+        {.file {sprintf('%sIN0_original_centromeres_for_FASTA.bed', out_path)}}
+      for splitting FASTA."
     )
   }
 }
-
-#########################################################################################################################################
-# how to run:
-#prepare_centromere_split = function(gtf_path, centromere_path, out_path, contig_prefix, mt_name, pt_name)
-# prepare_centromere_split(
-#   gtf_path = "/data/02_PROJECTS/sc_ShardSplitRef/01_Working_R_scripts/Toy_validate_GTF_script/toy_input/A0_toy_all_scenarios_2chr.gtf,   #A0_toy_all_scenarios.gtf",     ###/A0_toy_all_scenarios.gtf",
-#   centromere_path = "/data/02_PROJECTS/sc_ShardSplitRef/01_Working_R_scripts/Toy_validate_GTF_script/toy_input/A0_toy_centromeres_for_gtf.bed",
-#   out_path = "/data/02_PROJECTS/sc_ShardSplitRef/01_Working_R_scripts/Toy_validate_GTF_script/toy_input/",
-#    "CAJHDD",
-#    "Mt",
-#    "Pt"
-# )
